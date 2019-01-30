bf7a77134f961e6cbb8302da37817c407787c1ae
Peter Boos <peterdotboos@gmail.com>
2019-01-30 10:19:04 +0100

    Initial commit

---
+++ BotView/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BotView.Models;
+using Newtonsoft.Json;
+
+namespace BotView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PostQuery(string query)
+        {
+            /*
+             *  Todo:
+             *  
+             *  Do the bot stuffs here
+             *  parameter query is the user input to be evaluated
+             *  replace "Query successful" with bot response
+             */
+
+
+            var result = JsonConvert.SerializeObject(new
+            {
+                Message = "Query successful",
+                Query = query
+            });
+
+            return Ok(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BotView/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BotView.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
